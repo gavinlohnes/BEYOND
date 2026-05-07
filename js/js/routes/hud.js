be52e6b56b335ef088476d5a1168357642f113cb
gavinlohnes <gavin.lohnes@gmail.com>
2026-05-06 22:13:16 -0500

    Create hud.js

---
+++ js/js/routes/hud.js
@@ -0,0 +1,8 @@
+export default function renderHUD() {
+  return `
+    <div class="panel">
+      <div class="hdr-logo glow-faint">HUD</div>
+      <div class="sys-status dim">System online.</div>
+    </div>
+  `;
+}
